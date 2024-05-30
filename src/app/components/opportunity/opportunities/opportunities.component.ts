975046f4d0de51d7274fe7afb767611a5e066210
darshan <darshandumaraliya@gmail.com>
2024-05-30 22:06:15 +0530

    changes done.

--- src/app/components/opportunity/opportunities/opportunities.component.ts
+++ src/app/components/opportunity/opportunities/opportunities.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { pagination } from 'src/app/core/constant/pagination.constant';
+import { Payload } from 'src/app/core/constant/payload.const';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-opportunities',
@@ -6,5 +12,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./opportunities.component.scss']
 })
 export class OpportunitiesComponent {
+
+  showLoader: boolean = false;
+  categoryList: any = [];
+  industryList: any = [];
+  totalRecords: number = pagination.totalRecords;
+
+  projectList: any = [];
+  page: number = pagination.page;
+  pagesize = pagination.itemsPerPage;
+
+  constructor(
+    private projectService: ProjectService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private localStorageService: LocalStorageService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getProjectList();
+    // this.getcategoryList();
+    // this.getindustryList();
+  }
+
+  getcategoryList() {
+    this.showLoader = true;
+    this.projectService.getCategoryList().subscribe((response) => {
+      this.categoryList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.categoryList = response?.data;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  getindustryList() {
+    this.showLoader = true;
+    this.projectService.getIndustryList().subscribe((response) => {
+      this.industryList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.industryList = response?.data;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
+
+  getProjectList() {
+    this.showLoader = true;
+   // Payload.projectList.keyword = String(this.searchText?.value) || "";
+    Payload.projectList.page = String(this.page);
+    Payload.projectList.limit = String(this.pagesize);
+    Payload.projectList.applied = false;
+    Payload.projectList.sortlist = false;
+    this.projectService.getProjectList(Payload.projectList).subscribe((response) => {
+      this.projectList = [];
+      this.totalRecords = 0;
+      if (response?.status == true) {
+        this.showLoader = false;
+        this.projectList = response?.data?.data;
+      } else {
+        this.notificationService.showError(response?.message);
+        this.showLoader = false;
+      }
+    }, (error) => {
+      this.notificationService.showError(error?.message);
+      this.showLoader = false;
+    });
+  }
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environment/environment';
 import { Observable } from 'rxjs';
 
 export enum AuthEndPoint {
   LOGIN_USER = '/web-user/login',
   REGISTER = '/web-user/register',
-  PROJECT_LIST = '/project/list'
+  LATEST_PROJECT_LIST = '/project/list/latest',
+  CATEGORY_LIST = '/category/list',
+  INDUSTRY_LIST = '/industry/list',
+  PROJECT_LIST = '/project/list',
 }
 
 @Injectable({
@@ -39,8 +42,42 @@ export class ProjectService {
       .post<any>(this.baseUrl + AuthEndPoint.REGISTER, payload, { headers: this.getHeader() });
   }
 
-  projectList(payload :any): Observable<any> {
+  projectList(): Observable<any> {
     return this.httpClient
-      .post<any>(this.baseUrl + AuthEndPoint.REGISTER, payload, { headers: this.getHeader() });
+      .get<any>(this.baseUrl + AuthEndPoint.LATEST_PROJECT_LIST, { headers: this.getHeader() });
   }
+
+  getCategoryList(): Observable<any> {
+    return this.httpClient
+      .get<any>(this.baseUrl + AuthEndPoint.CATEGORY_LIST, { headers: this.getHeader() });
+  }
+
+  getIndustryList(): Observable<any> {
+    return this.httpClient
+      .get<any>(this.baseUrl + AuthEndPoint.INDUSTRY_LIST, { headers: this.getHeader() });
+  }
+
+  getProjectList(params: { keyword: string, page: string, limit: string, status: string }): Observable<any> {
+    const url = `${this.baseUrl}${AuthEndPoint.PROJECT_LIST}`;
+
+    let queryParams = new HttpParams();
+    queryParams = queryParams.set('keyword', params?.keyword || '');
+    queryParams = queryParams.set('page', params?.page);
+    queryParams = queryParams.set('limit', params?.limit);
+    // if (params?.applied) {
+    //   queryParams = queryParams.set('applied', params?.applied);
+    // }
+    // if (params?.sortlist) {
+    //   queryParams = queryParams.set('sortlist', params?.sortlist);
+    // }
+    // if (params?.match) {
+    //   queryParams = queryParams.set('match', params?.match);
+    // }
+    // if (params?.status) {
+    //   queryParams = queryParams.set('status', params?.status);
+    // }
+    return this.httpClient.get<any>(url, { params: queryParams });
+  }
+
+
 }
